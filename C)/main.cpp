5ef486d8a8fe4a52f7ba29e5ca2460928773da2d
Radoslav Karatanev <rmkrmkrmk13@gmail.com>
2023-01-09 01:15:44 +0200

    First try at the Box class

--- C)/main.cpp
+++ C)/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <fstream>  
+#include <vector>
 
 class Box{
     std::string name;
-    Box* inside;
+    std::vector<std::string> items;
+    std::vector<Box> inside;
+
+    Box(const std::string& name,  const std::vector<std::string> items, const std::vector<Box> inside) : name(name), items(items), inside(inside) {};
 };
 
+void doStuff(std::fstream& f){
+
+}
+
 int main(){
-    std::cout <<"Hello World!";
+    std::fstream file("boxesList.txt");
+    doStuff(file);
+    file.close();
 }
