cf9bfeb03cdc940dd82fba7b1305a37ac3d86193
Radoslav Karatanev <rmkrmkrmk13@gmail.com>
2023-01-09 01:39:26 +0200

    Very basic example

--- C)/main.cpp
+++ C)/main.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 #include <fstream>  
-#include <vector>
+#include <queue>
 
-class Box{
+struct Box{
     std::string name;
-    std::vector<std::string> items;
-    std::vector<Box> inside;
+    std::queue<std::string> items;
+    std::queue<Box> inside;
 
-    Box(const std::string& name,  const std::vector<std::string> items, const std::vector<Box> inside) : name(name), items(items), inside(inside) {};
+    Box() : name(""), items(), inside() {};
+    Box(const std::string& name,  const std::queue<std::string> items, const std::queue<Box> inside) : name(name), items(items), inside(inside) {};
 };
 
 void doStuff(std::fstream& f){
+    int b;
+    f >> b;
+
+    std::cout << b << std::endl;
+    Box mainBox;
+
+    std::string temp;
+    int n;
+    f >> temp;
+    mainBox.name = temp;
+    f >> n;
+    for(std::size_t i = 0; i < n; ++i){
+        f >> temp;
+        mainBox.items.push(temp);
+    }
+
+    f >> n;
+    for(std::size_t i = 0; i < n; ++i){
+        f >> temp;
+        Box newBox;
+        newBox.name = temp;
+        mainBox.inside.push(newBox);
+    }
+
+
+    while(!mainBox.items.empty()){
+        std::cout << mainBox.items.front() << " ";
+        mainBox.items.pop();
+    }
+    std::cout << std::endl;
+
+    while(!mainBox.inside.empty()){
+        std::cout << mainBox.inside.front().name << " ";
+        mainBox.inside.pop();
+    }
+    std::cout << std::endl;
+
 
 }
 
